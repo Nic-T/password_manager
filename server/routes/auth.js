383af970f9e8ced3afc4f7891fbc90bf91ed9453
Nic-T <nicolastantau2003@gmail.com>
2022-08-13 21:46:23 +0300

    The basic auth routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { register, login } = require("../controllers/user");
+
+const { register, login, logout } = require("../controllers/auth");
 
 router.post("/register", register);
 
 router.post("/login", login);
+
+router.get("/logout ", logout);
 
 module.exports = router;
