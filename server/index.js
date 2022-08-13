946c22dc396e908df457ac13934066702483121b
Nic-T <nicolastantau2003@gmail.com>
2022-08-13 21:47:48 +0300

    Added express sessions with  redis integration

--- server/index.js
+++ server/index.js
@@ -1,18 +1,44 @@
 const express = require("express");
 const cors = require("cors");
-let bodyParser = require("body-parser");
+const redis = require("redis");
+const connectRedis = require("connect-redis");
+const dotenv = require("dotenv").config();
+const session = require("express-session");
+
+let db = require("./models");
+
+const SESSION_SECRET = process.env.SESSION_SECRET;
 
 const app = express();
 const port = process.env.PORT || 3100;
 
-const userRoutes = require("./routes/user");
+// app.set('trust proxy',1)
 
-let db = require("./models");
+const RedisStore = connectRedis(session);
+const redisClient = redis.createClient({
+  port: 6379,
+  host: "localhost",
+});
 
+const userRoutes = require("./routes/auth");
+
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      httpOnly: true,
+      maxAge: 1000 * 60 * 30,
+    },
+  })
+);
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/user", userRoutes);
+app.use("/api/auth", userRoutes);
 
 db.sequelize.sync({ force: true }).then(function () {
   app.listen(port, () => {
