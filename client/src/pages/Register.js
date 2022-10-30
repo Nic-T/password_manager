6608161a0a59293811512c00f0a175ea47425f74
Nic-T <nicolastantau2003@gmail.com>
2022-10-30 19:05:50 +0200

    Finished the design of the register page

---
+++ client/src/pages/Register.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { useState } from "react";
+
+function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = { email: email, password: password };
+
+    fetch("http://localhost:3100/api/auth/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+  };
+
+  return (
+    <div class="w-screen	h-screen flex justify-center bg-cover bg-bg1">
+      <div class="  flex flex-col  w-3/12 h-3/6 self-center p-16 shadow-xl rounded-lg border-slate-50 border space-y-4 bg-white">
+        <div>
+          <h1 class="text-2xl"> Sign Up</h1>
+          <p class="my-2">
+            Already have an account?{" "}
+            <a href="" class="text-blue-700">
+              Sign In
+            </a>
+          </p>
+        </div>
+
+        <form onSubmit={handleSubmit} class="flex flex-col space-y-4 ">
+          <div class="inline-block p-1.5 border rounded-lg">
+            <label class="block text-xs text-gray-500">Email</label>
+            <input
+              class="block leading-5  outline-0 border-0 w-full p-1"
+              type="text"
+              value={email}
+              name="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div class="inline-block p-1.5 border rounded-lg ">
+            <label class="block text-xs text-gray-500">Password</label>
+            <input
+              class="block leading-5  outline-0 border-0 w-full p-1"
+              value={password}
+              type="password"
+              name="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <button
+            class="p-2 bg-blue-400 rounded-md text-slate-50 shadow-md shadow-blue-300"
+            type="submit"
+            value="Submit"
+          >
+            Submit!
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
