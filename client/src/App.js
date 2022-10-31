54464478041c25a5ffb87a6cd36129f98ed97008
Nic-T <nicolastantau2003@gmail.com>
2022-11-01 00:19:57 +0200

    misc

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import Register from "./pages/Register";
-
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Register />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/register" element={<Register />}></Route>
+        <Route exact path="/login" element={<Login />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         bg1: "url('/src/images/img1.svg')",
+        bg2: "url('/src/images/bg2.svg')",
       },
     },
   },
